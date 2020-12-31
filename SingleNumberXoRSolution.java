fc50d2a265bff54763637c0e671d4065fda8103a
Vijaya Laxmi <vijaya2798@gmail.com>
2020-12-31 20:20:32 +0530

    Vijaya (#3)
    * Create SingleNumberListSol.java
    
    This file contains solution to LeetCode's Single number question's solution. The solution is the basic approach with Time Complexity of O(n2).
    
    * Create SingleNumberHashMapSol.java
    
    This gives solution to Single Number question's solution. The approach used is HashMap which used to keep the numbers in the array as key and their occurence as count.
    
    * Create SingleNumberXoRSolution.java
    
    This gives solution to Single Number question of leetcode. Here we use XOR of two numbers to find whether the number has been encountered before or not.

---
+++ SingleNumberXoRSolution.java
@@ -0,0 +1,10 @@
+class SingleNumberXoRSolution {
+    public int singleNumber(int[] nums) {
+        int a =0;
+        for( int i: nums){
+            a=a^i;
+        }
+        return a;
+        
+    }
+}
