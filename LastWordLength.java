cc1c2df811b42dc8ef5fc28b69338b11ac1ad30b
Vijaya Laxmi <vijaya2798@gmail.com>
2021-03-02 13:55:14 +0530

    Create LastWordLength.java
    Solution with built-in String functions in java

---
+++ LastWordLength.java
@@ -0,0 +1,8 @@
+class LastWordLength {
+    public int lengthOfLastWord(String s) {
+        String[] str = s.split(" ");
+        if(str.length == 0) return 0;
+        
+       return str[str.length-1].length();
+    }
+}
