e561983336f1c34b470ff68ae98180206a3746e3
Vijaya Laxmi <vijaya2798@gmail.com>
2021-03-16 21:49:26 +0530

    Create RotateNinty.java
    use transpose & reflect

---
+++ RotateNinty.java
@@ -0,0 +1,28 @@
+class RotateNinty {
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reflect(matrix);
+    }
+    
+    public void transpose(int[][] matrix){
+        int n = matrix.length;
+        for(int i = 0; i<n; i++){
+            for(int j=i; j<n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+    
+    public void reflect(int[][] matrix){
+        int n = matrix.length;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+        }
+    }
+}
