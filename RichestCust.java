0a0fffc590d1e4cab0decaaf777b732b87bd6165
Vijaya Laxmi <vijaya2798@gmail.com>
2021-05-06 22:45:42 +0530

    Create RichestCust.java
    Solution to Richest Customer .

---
+++ RichestCust.java
@@ -0,0 +1,15 @@
+class RichestCust {
+    public int maximumWealth(int[][] accounts) {
+        int maxWealth = 0;
+        for(int i=0; i<accounts.length; i++){
+            int sum  =0 ;
+            for(int j=0; j<accounts[i].length; j++){
+                sum  =  sum + accounts[i][j];
+            }
+            if(maxWealth<sum){
+                maxWealth = sum;
+            }
+        }
+        return maxWealth;
+    }
+}
