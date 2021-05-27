0d0039a2697fa296ca53e9519bc87759fc624ccc
Vijaya Laxmi <vijaya2798@gmail.com>
2021-05-28 00:09:34 +0530

    Create NumberOfSteps.js

---
+++ NumberOfSteps.js
@@ -0,0 +1,18 @@
+ /**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let step = 0;
+    while(num>0){
+        if(num%2 == 0) {
+            num = num/2; 
+            step++;
+        }
+        else {
+            num = num-1;
+            step++;
+        }
+    }
+    return step;
+};
