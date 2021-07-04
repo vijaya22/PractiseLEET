3d446b329b2d95e63bc8fb40d5df4ae1acaf401f
Vijaya Laxmi <vijaya2798@gmail.com>
2021-07-04 14:34:55 +0530

    Create TwoSum.js

---
+++ TwoSum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+    for(let i=0;i<nums.length; i++){
+        let complement = target - nums[i];
+        if(map.has(complement)){
+            return [map.get(complement),i];
+        }
+        else map.set(nums[i],i);
+    }
+    return [-1,-1];
+};
