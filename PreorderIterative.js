38728f5943509fad9294ee8b24da2a4788d05326
Vijaya Laxmi <vijaya2798@gmail.com>
2021-07-25 15:25:41 +0530

    Create PreorderIterative.js

---
+++ PreorderIterative.js
@@ -0,0 +1,33 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function(root) {
+    // time complexity = O(n)
+    // space complexity = O(n)
+    if(!root) return null;
+    let stack = [];
+    let output = [];
+    stack.push(root);
+    
+    while(stack.length){
+        let node = stack.pop();
+        output.push(node.val);
+        node.children.reverse();
+        for(let item of node.children){
+            if(item!=null){
+                stack.push(item);
+            }
+        }
+    }
+    return output;
+    
+};
