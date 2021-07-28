0199f095caf28bc367c8d7c5d33d407deaa25838
Vijaya Laxmi <vijaya2798@gmail.com>
2021-07-28 22:14:51 +0530

    Create SearchBSTRecursive.js

---
+++ SearchBSTRecursive.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    // search a node in BST with node's value == val
+    // return subtree with that node
+    // time complexity - O(h) // worst case - O(n) - skewed
+    // space coplexity - O(h) // worst case -O(n) - skewed
+    if(!root) return [];
+    if(root.val == val) return root;
+    if(val < root.val){
+      return searchBST(root.left, val);
+    }
+    if(val > root.val){
+        return searchBST(root.right, val)
+    }
+};
