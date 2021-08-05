b6408f39dbfb11361b045b1e5a8c702e756cc7bd
Vijaya Laxmi <vijaya2798@gmail.com>
2021-08-05 23:17:37 +0530

    Create FindPeakElementBinaryIterative.js

---
+++ FindPeakElementBinaryIterative.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    // use binary search to find peak
+    // time complexity - O(log n)
+    // space complexity - O(1)
+    let left = 0;
+    let right = nums.length-1;
+    while(left<right){
+        let mid = Math.floor((left+right)/2);
+        if(nums[mid] > nums[mid+1]){
+            right = mid;
+        }
+        else left = mid+1;
+    }
+    return left;
+};
