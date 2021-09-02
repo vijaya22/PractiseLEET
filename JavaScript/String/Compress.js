0a73c401614d98daa957547354a718219a9aec57
Vijaya Laxmi <vijaya2798@gmail.com>
2021-09-03 00:37:57 +0530

    Create Compress.js

---
+++ JavaScript/String/Compress.js
@@ -0,0 +1,30 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+    // used new array to give compressed string
+    // time complexity - O(n)
+    // space complexity - O(n)
+    let currCount = 0;
+    let currChar = chars[0];
+    let compressedString = [];
+    
+    for(let i=0; i<chars.length; i++){
+        if(currChar == chars[i]){
+            console.log('if:')
+            currCount ++;
+        }
+        else{
+            compressedString.push(currChar);
+            currChar = chars[i];
+            if(currCount>1){
+              compressedString.push(""+currCount);
+            }
+            currCount = 1;
+        }
+    }
+    compressedString.push(currChar);
+    currCount > 1 ? compressedString.push(currCount) : '';
+    return compressedString.length;
+};
