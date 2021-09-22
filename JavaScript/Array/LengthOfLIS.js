482aef600874770059184d0788dba36c32581d60
Vijaya Laxmi <vijaya2798@gmail.com>
2021-09-22 23:53:16 +0530

    Create LengthOfLIS.js

---
+++ JavaScript/Array/LengthOfLIS.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    let dp = new Array(nums.length).fill(1);
+    for(let i=1; i<nums.length; i++){
+        for(let j = 0; j<i; j++){
+            if(nums[i] > nums[j]){
+                dp[i] = Math.max(dp[i], dp[j]+1);
+            }
+        }
+    }
+    let longest = 0;
+    for(let len of dp){
+        longest = Math.max(longest,len);
+    }
+    return longest;
+};
